68ba2f8f53a78cbf5c83af600e66f9c380c6db05
tianhua87 <787156584@qq.com>
2017-04-07 20:42:04 +0800

    Initial commit

---
+++ src/Animal.java
@@ -0,0 +1,7 @@
+/**
+ * Created by wen on 2017/4/7.
+ */
+public interface Animal {
+
+    public void say();
+}
---
+++ src/HelloWorld.java
@@ -0,0 +1,13 @@
+/**
+ * Created by wen on 2017/4/7.
+ */
+public class HelloWorld {
+
+    public static void main(String args[]){
+
+        System.out.println("Hello This World");
+        System.out.println("Hello This World1");
+    }
+
+
+}
